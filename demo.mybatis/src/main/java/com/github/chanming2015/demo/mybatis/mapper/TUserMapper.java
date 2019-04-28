8ab4ad19e8d65d1c2ca5697b5eb4ca09f839c78a
chanming2015 <476076812@qq.com>
2019-04-28 22:23:35 +0800

    mybatis init.

---
+++ demo.mybatis/src/main/java/com/github/chanming2015/demo/mybatis/mapper/TUserMapper.java
@@ -0,0 +1,17 @@
+package com.github.chanming2015.demo.mybatis.mapper;
+
+import com.github.chanming2015.demo.mybatis.entity.TUser;
+
+public interface TUserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(TUser record);
+
+    int insertSelective(TUser record);
+
+    TUser selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(TUser record);
+
+    int updateByPrimaryKey(TUser record);
+}
