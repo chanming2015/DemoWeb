17b11b3ce85fd62a922988eeb404b754e1a035e5
chanming2015 <476076812@qq.com>
2019-04-30 18:40:26 +0800

    springboot demo init.

---
+++ springboot.demo/src/main/java/com/github/chanming2015/springboot/demo/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.github.chanming2015.springboot.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController
+{
+    @RequestMapping("/hello")
+    public String index()
+    {
+        return "Hello World";
+    }
+}
+
