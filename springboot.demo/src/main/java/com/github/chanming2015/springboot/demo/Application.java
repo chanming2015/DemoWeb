a3e0d6faeeb830d997afda72c644e76541b2c52a
chanming2015 <476076812@qq.com>
2020-04-29 14:09:34 +0800

    change

--- springboot.demo/src/main/java/com/github/chanming2015/springboot/demo/Application.java
+++ springboot.demo/src/main/java/com/github/chanming2015/springboot/demo/Application.java
@@ -2,12 +2,45 @@ package com.github.chanming2015.springboot.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-public class Application {
+@EnableSwagger2
+@EnableWebSecurity
+public class Application extends WebSecurityConfigurerAdapter
+{
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Application.class, args);
+    }
 
+    @Bean
+    public Docket createApi()
+    {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select().apis(RequestHandlerSelectors.basePackage("com.github.chanming2015.springboot.demo.controller")).paths(PathSelectors.any()).build();
+    }
+
+    private ApiInfo apiInfo()
+    {
+        return new ApiInfoBuilder().title("springboot demo document api").description("Swagger2 Document Api").version("1.0.0").contact(new Contact("xumaosen", "", "")).build();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+        http.authorizeRequests().antMatchers("/actuator/info", "/actuator/health").permitAll().anyRequest().authenticated().and().formLogin().and().httpBasic();
+    }
 }
--- springboot.demo/src/main/java/com/github/chanming2015/springboot/demo/controller/HelloWorldController.java
+++ springboot.demo/src/main/java/com/github/chanming2015/springboot/demo/controller/HelloWorldController.java
@@ -1,12 +1,17 @@
 package com.github.chanming2015.springboot.demo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
 
 @RestController
 public class HelloWorldController
 {
-    @RequestMapping("/hello")
+    @ApiOperation(value = "Hello World API")
+    @ApiResponse(code = 200, message = "Search Success")
+    @GetMapping("/hello")
     public String index()
     {
         return "Hello World";
